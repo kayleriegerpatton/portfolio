9ff4f58c779fc7b4bc29185124c9f648c3a36f03
Kayle Rieger Patton <kayle.patton22@gmail.com>
2024-02-15 16:38:32 +0100

    add header elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,48 @@
 import React from 'react';
+import Header from './components/Header';
+import './App.css';
+
+// TODO: change window location to hash
+function locationHashChanged() {
+	const hashId = window.location.hash;
+	console.log(`Change window location to: ${hashId}`);
+
+	// ! cant get elementById with react
+	// const element = document.getElementById(hashId);
+	// console.log(element);
+	// element.scrollIntoView({
+	// 	behavior: 'smooth',
+	// 	block: 'start',
+	// 	inline: 'nearest',
+	// });
+}
+
+window.onhashchange = locationHashChanged;
+
+// function ScrollToAnchor() {
+//   const location = useLocation();
+//   const lastHash = React.useRef('');
+
+//   // listen to location change using useEffect with location as dependency
+//   useEffect(() => {
+//     if (location.hash) {
+//       lastHash.current = location.hash.slice(1); // safe hash for further use after navigation
+//     }
+
+//     if (lastHash.current && document.getElementById(lastHash.current)) {
+//       setTimeout(() => {
+//         document
+//           .getElementById(lastHash.current)
+//           ?.scrollIntoView({ behavior: 'smooth', block: 'start' });
+//         lastHash.current = '';
+//       }, 100);
+//     }
+//   }, [location]);
+
+//   return null;
+// }
 
 function App() {
-	return <h1>Hello world. HI</h1>;
+	return <Header />;
 }
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,20 @@
 import React from 'react';
+import './Header.css';
 
-const Header = () => {
-	<h1>Hello</h1>;
-};
+function Header() {
+	return (
+		<header>
+			<p>Kayle</p>
+			<nav>
+				<a href="#about">About</a>
+				<a href="#projects">Projects</a>
+				<a href="#contact">Contact Me</a>
+				<a href="https://www.canva.com/design/DAE4Ifi_4Ok/WT7Oh8YsOt_JQm9Z-R28dw/view?utm_content=DAE4Ifi_4Ok&utm_campaign=designshare&utm_medium=link&utm_source=editor">
+					CV
+				</a>
+			</nav>
+		</header>
+	);
+}
 
 export default Header;
-
-// import React from 'react'
-// import { Link } from 'react-router-dom'
-// import './Header.css'
-
-// const Header = () => (
-//   <div className='header'>
-//     <div>
-//       <Link to='/' className='header__link' aria-label='Link to home page'>
-//         <img src={logo} className='header__logo' alt='Metrik logo' />
-//       </Link>
-//     </div>
-
-//     <div className='header-link-container'>
-//       <Link to='/getting-started' className='header__link'>
-//         Getting Started
-//       </Link>
-//       <Link to='/slackbot' className='header__link'>
-//         Slack Bot
-//       </Link>
-//       <Link to='/help' className='header__link'>
-//         Help
-//       </Link>
-//     </div>
-
-//   </div>
-// )
-
-// module.exports = Header
