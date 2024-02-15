f228a6699734059e8f350842b1143f724e333788
Kayle Rieger Patton <kayle.patton22@gmail.com>
2024-02-15 16:41:57 +0100

    remove comment

--- src/App.jsx
+++ src/App.jsx
@@ -19,29 +19,6 @@ function locationHashChanged() {
 
 window.onhashchange = locationHashChanged;
 
-// function ScrollToAnchor() {
-//   const location = useLocation();
-//   const lastHash = React.useRef('');
-
-//   // listen to location change using useEffect with location as dependency
-//   useEffect(() => {
-//     if (location.hash) {
-//       lastHash.current = location.hash.slice(1); // safe hash for further use after navigation
-//     }
-
-//     if (lastHash.current && document.getElementById(lastHash.current)) {
-//       setTimeout(() => {
-//         document
-//           .getElementById(lastHash.current)
-//           ?.scrollIntoView({ behavior: 'smooth', block: 'start' });
-//         lastHash.current = '';
-//       }, 100);
-//     }
-//   }, [location]);
-
-//   return null;
-// }
-
 function App() {
 	return <Header />;
 }
