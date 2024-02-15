c6f6046088477b51f050f42b7cd1762f60a14fc0
Kayle Rieger Patton <kayle.patton22@gmail.com>
2024-02-15 17:34:21 +0100

    arranged header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
 			<nav>
 				<a href="#about">About</a>
 				<a href="#projects">Projects</a>
-				<a href="#contact">Contact Me</a>
+				<a href="#contact">Contact</a>
 				<a href="https://www.canva.com/design/DAE4Ifi_4Ok/WT7Oh8YsOt_JQm9Z-R28dw/view?utm_content=DAE4Ifi_4Ok&utm_campaign=designshare&utm_medium=link&utm_source=editor">
 					CV
 				</a>
