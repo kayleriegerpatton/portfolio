074d7136f7bcd2f7eb83fc8f561ae8bb5354d08a
Kayle Rieger Patton <kayle.patton22@gmail.com>
2024-02-16 09:30:41 +0100

    arranged footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faSquareGithub } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import './Footer.css';
 
 function Footer() {
-	return <p>Footer</p>;
+	return (
+		<footer>
+			<div>
+				<FontAwesomeIcon icon={faSquareGithub} className="icon" size="2xl" />
+				<FontAwesomeIcon icon={faLinkedin} className="icon" size="2xl" />
+				<FontAwesomeIcon icon={faEnvelope} className="icon" size="2xl" />
+			</div>
+			<p>Built by Kayle Rieger Patton</p>
+		</footer>
+	);
 }
 export default Footer;
