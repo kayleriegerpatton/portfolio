e571efd0baf596b11117a604915344bc7f466736
Kayle Rieger Patton <kayle.patton22@gmail.com>
2024-02-16 13:27:48 +0100

    arranged hero text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,9 +4,18 @@ import './Hero.css';
 function Hero() {
 	return (
 		<div id="hero">
-			<p>I&apos;m a</p>
-			<p>Software Engineer</p>
-			<p>with an eye for design</p>
+			{/* semantic h1 is visually hidden, but available to screen readers */}
+			<h1 className="hidden">
+				I&apos;m a Software Engineer with an eye for design
+			</h1>
+			<p id="hero-text-1">I&apos;m a</p>
+			<p className="hero-emphasis" id="hero-emphasis-1">
+				Software
+			</p>
+			<p className="hero-emphasis" id="hero-emphasis-2">
+				Engineer
+			</p>
+			<p id="hero-text-2">with an eye for design</p>
 		</div>
 	);
 }
