c485f65f3b276d6f71c113156ab543f8e181044b
Kayle Rieger Patton <kayle.patton22@gmail.com>
2024-02-18 16:29:00 +0100

    add hash links to nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+import { HashLink } from 'react-router-hash-link';
 import './Nav.css';
 
 function Nav() {
 	return (
 		<nav>
-			<a href="#about">About</a>
-			<a href="#projects">Projects</a>
-			<a href="#contact">Contact</a>
-			<a
+			<HashLink to="#about-section">About</HashLink>
+			<HashLink to="#projects-section">Projects</HashLink>
+			<HashLink to="#contact-section">Contact</HashLink>
+			<Link
 				href="https://www.canva.com/design/DAE4Ifi_4Ok/WT7Oh8YsOt_JQm9Z-R28dw/view?utm_content=DAE4Ifi_4Ok&utm_campaign=designshare&utm_medium=link&utm_source=editor"
 				target="_blank"
 				rel="noreferrer"
@@ -20,7 +22,7 @@ function Nav() {
 					className="nav-icon"
 					size="sm"
 				/>
-			</a>
+			</Link>
 		</nav>
 	);
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './App.jsx';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>
+);
+
+/* root.render(<RouterProvider router={router} />) */
