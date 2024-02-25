9bb09515ee319808183227f5ad1e21c3ff482209
Kayle Rieger Patton <kayle.patton22@gmail.com>
2024-02-25 18:58:32 +0100

    added a tags to footer icons

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,14 +8,37 @@ function Footer() {
 	return (
 		<footer>
 			<div>
-				{/* TODO: add anchor tags */}
-				<FontAwesomeIcon
-					icon={faSquareGithub}
-					className="footer-icon"
-					size="2xl"
-				/>
-				<FontAwesomeIcon icon={faLinkedin} className="footer-icon" size="2xl" />
-				<FontAwesomeIcon icon={faEnvelope} className="footer-icon" size="2xl" />
+				<a
+					aria-label="Kayle's GitHub"
+					href="https://github.com/kayleriegerpatton"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<FontAwesomeIcon
+						icon={faSquareGithub}
+						className="footer-icon"
+						size="2xl"
+					/>
+				</a>
+				<a
+					aria-label="Kayle's LinkedIn"
+					href="https://linkedin.com/in/kaylerieger"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<FontAwesomeIcon
+						icon={faLinkedin}
+						className="footer-icon"
+						size="2xl"
+					/>
+				</a>
+				<a aria-label="email Kayle" href="mailto:kayle.patton22@gmail.com">
+					<FontAwesomeIcon
+						icon={faEnvelope}
+						className="footer-icon"
+						size="2xl"
+					/>
+				</a>
 			</div>
 			<p>Built by Kayle Rieger Patton</p>
 		</footer>
