570fa1013597d38b06edae3dde59c0639ca974cc
Kayle Rieger Patton <kayle.patton22@gmail.com>
2024-03-15 09:40:13 +0100

    removed scrollWithOffset function

--- src/components/ProjectsSection/ProjectsSection.jsx
+++ src/components/ProjectsSection/ProjectsSection.jsx
@@ -3,18 +3,12 @@ import React, { useState } from 'react';
 import './ProjectsSection.css';
 import ProjectCard from '../ProjectCard/ProjectCard';
 import projects from '../../data/projects';
-import scrollWithOffset from '../../helpers/scrollWithOffset';
 
 function ProjectsSection() {
 	const [projectsExpanded, setProjectsExpanded] = useState(false);
 
 	const expandProjects = () => {
 		setProjectsExpanded(!projectsExpanded);
-		// if projects are being expanded, don't scroll up
-		if (projectsExpanded === true) {
-			const element = document.getElementById('projects');
-			scrollWithOffset(element);
-		}
 	};
 
 	const unarchivedProjects = projects.filter(
