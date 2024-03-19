35ac219c5b0c9f5c1ea5d58e9d178821cd5036d0
Kayle Rieger Patton <kayle.patton22@gmail.com>
2024-03-19 12:25:45 +0100

    added portfolio repo name to router basename

--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,7 @@ import App from './App';
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
-	<BrowserRouter future={{ v7_startTransition: true }}>
+	<BrowserRouter basename="/portfolio/" future={{ v7_startTransition: true }}>
 		<App />
 	</BrowserRouter>
 );
