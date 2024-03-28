2e9921995458891bcbc9319f2de5cc03c322afbc
Kayle Rieger Patton <kayle.patton22@gmail.com>
2024-03-28 15:11:45 +0100

    mocked App child components for testing

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,9 +3,10 @@ import '@testing-library/jest-dom';
 import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
 
+import Home from './pages/Home';
 import App from './App';
 // TODO mock children so console.error resolves
-// jest.mock('./components/Header.jsx', () => () => <div data-tstid="header" />);
+jest.mock('./pages/Home');
 
 describe('App', () => {
 	it('should render the header logo link', () => {
@@ -18,6 +19,8 @@ describe('App', () => {
 		// screen.debug();
 		const heading = screen.getByRole('link', { name: 'Kayle' });
 		expect(heading).toBeInTheDocument();
+		expect(Home.mock.calls).toHaveLength(1);
+		expect(Home).toHaveBeenCalled();
 	});
 
 	// TODO: test failed to render (show 404 page instead)
