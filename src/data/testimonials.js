d86e5d5bfd5d9b20ca2755283dbbc58410b0fa52
Kayle Rieger Patton <kayle.patton22@gmail.com>
2024-04-26 10:27:30 +0200

    edit testimonials text

--- src/data/testimonials.js
+++ src/data/testimonials.js
@@ -17,7 +17,7 @@ const testimonials = [
 		company: 'BBC',
 		image: '',
 		altText: 'Yav',
-		text: 'The BBC Cloud Platform team is responsible for critical systems and tools which deal with service deployments to AWS, large scale PKI automation, AWS account management and security. Our deployment system deals with thousands of EC2 and Lambda deployments daily across hundreds of AWS accounts. Kayle demonstrated exceptional contributions to our team, particularly in her involvement in developing our deployment system CLI. This project demanded meticulous design and implementation skills, enabling hundreds of developers to seamlessly access the richness of our system. Kayle is very attentive during meetings and conversations, has an acute attention to detail and importantly puts in the work to get stuff done. I have full confidence in her ability to make significant contributions to any team, regardless of the domain.',
+		text: 'Kayle demonstrated exceptional contributions to our team, particularly in her involvement in developing our deployment system CLI. This project demanded meticulous design and implementation skills, enabling hundreds of developers to seamlessly access the richness of our system. Kayle is very attentive during meetings and conversations, has an acute attention to detail and importantly puts in the work to get stuff done.',
 	},
 	{
 		id: 3,
@@ -27,7 +27,7 @@ const testimonials = [
 		company: 'BBC',
 		image: '',
 		altText: 'anonymous profile image',
-		text: 'Looking through the new [CLI] docs and honestly they look amazing, and so consistent and much easier to understand than probably 99% of the documentation I normally read, and it looks like a serious amount of effort went into them... It means a lot to me when someone puts in that kind of effort and creates something that helps other people, especially when it is something often ignored or done after the fact with little thought or care like docs are, so thank you a huge amount.',
+		text: 'Looking through the new [CLI] docs and honestly they look amazing, and so consistent and much easier to understand than probably 99% of the documentation I normally read... It means a lot to me when someone puts in that kind of effort and creates something that helps other people, especially when it is something often ignored or done after the fact with little thought or care like docs are, so thank you a huge amount.',
 	},
 ];
 
