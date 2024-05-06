ec10fc366f8749f57e090d99d659eb1af47f7df2
Kayle Rieger Patton <kayle.patton22@gmail.com>
2024-05-06 20:23:02 +0200

    fix polyfill error on react-slick carousel

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 
 import Home from './pages/Home';
 import App from './App';
-// TODO mock children so console.error resolves
+
 jest.mock('./pages/Home');
 
 describe('App', () => {
